d3f1d852638a95f5b0f3695bcc7dc3df3596ffbc
Sajid Jaber <sajidjaber@yahoo.com>
2022-07-03 08:45:00 -0500

    heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,6 @@
-const STATIC_CACHE = 'static_cache_v1'
-const DATA_CACHE = 'data_cache_v1'
+const APP_PREFIX = 'Budget-Tracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION
 
 const FILES_TO_CACHE = [
     './index.html',
@@ -17,64 +18,45 @@ const FILES_TO_CACHE = [
     './icons/icon-72x72.png'
 ]
 
-const swIcon = `🤖 (SW): `
-
-self.addEventListener('install', event => {
-  console.log(`${swIcon} Installing...`)
-  event.waitUntil(
-    caches.open(STATIC_CACHE)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+          console.log('installing... ' + CACHE_NAME);
+          return cache.addAll(FILES_TO_CACHE)
+      })
   )
-  self.skipWaiting() // forces the waiting sevice worker to become the active service worker: https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerGlobalScope/skipWaiting
-})
+});
 
-self.addEventListener('activate', event => {
-  console.log(`${swIcon} Activating...`)
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== STATIC_CACHE) {
-            return caches.delete(key)
-          }
-        })
-      )
-    })
-  )
-  self.clients.claim() // active service worker to set itself as the controller for all clients within its scope: https://developer.mozilla.org/en-US/docs/Web/API/Clients/claim
-})
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+      caches.keys().then(function (keyList) {
+          let cacheKeeplist = keyList.filter(function (key) {
+              return key.indexOf(APP_PREFIX);
+          });
+          cacheKeeplist.push(CACHE_NAME);
 
-self.addEventListener('fetch', event => {
-  console.log(`${swIcon} Requesting `, event.request)
-  // Requests for data
-  // Strategy: Network-first, fallback to cache
-  if (event.request.url.includes('dummyjson.com') && event.request.method === 'GET') {
-    event.respondWith(
-      // open caches
-      caches.open(DATA_CACHE)
-        .then(cache => {
-          // try network with a fetch request
-          return fetch(event.request)
-            .then(response => {
-              // if success
-              if (response.status === 200) {
-                // save response in cache
-                cache.put(event.request.url, response.clone())
+          return Promise.all(keyList.map(function (key, i) {
+              if (cacheKeeplist.indexOf(key) === -1) {
+                  console.log('deleting cache... ' + keyList[i] );
+                  return caches.delete(keyList[i]);
               }
-              return response
-            })
-            // if fails pull last saved data from cache
-            .catch(() => caches.match(event.request))
-        })
-        .catch(err => console.log(err))
-    )
-    return
-  }
+          }));
+      })
+  )
+});
 
-  // Request for static assets (.html, .css, .js, .jpg, .png)
-  // Strategy: Cache-first, fallback to Network
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request...' + e.request.url);
+  e.respondWith(
+      caches.match(e.request).then(function (request) {
+          if (request) { // if cache is available, respond with cache
+              console.log('responding with cache : ' + e.request.url);
+              return request
+          } else {       // if there are no cache, try fetching request
+              console.log('file is not cached, fetching : ' + e.request.url);
+              return fetch(e.request)
+          }
+
+      })
   )
 })
